e2beb3642a43b8f7e2dd1ab116fdc7c0da624d6c
white <grigsgt@gmail.com>
2026-02-23 22:05:08 +0300

    Create base session functions, create, get, validate, delete

---
+++ server/src/modules/auth/service.ts
@@ -0,0 +1,116 @@
+import type {PrismaClient} from '../../../prisma/generated/client'
+
+interface ISession {
+    id: string;
+    secretHash: string;
+    createdAt: Date;
+}
+
+interface ISessionWithToken extends ISession {
+    token: string;
+}
+
+const SESSION_EXPIRES_IN_SECONDS = 60 * 60 * 24;
+
+async function createSession(db: PrismaClient): Promise<ISessionWithToken> {
+    const id = generateSecureRandomString()
+    const secret = generateSecureRandomString()
+    const secretHash = await hashSecret(secret)
+
+    const token = `${id}.${secret}`
+
+    const newSession = await db.session.create({
+        data: {
+            id,
+            secretHash
+        }
+    })
+
+    return {
+        id,
+        secretHash,
+        token,
+        createdAt: newSession.createAt
+    }
+}
+
+async function validateSession(db: PrismaClient, token: string): Promise<ISession | null> {
+    const tokenParts = token.split('.')
+    if (tokenParts.length !== 2)
+        return null
+
+    const sessionId = tokenParts[0];
+    const sessionSecret = tokenParts[1];
+
+    const session = await getSession(db, sessionId);
+
+    if (!session)
+        return null
+
+    const tokenSecretHash = await hashSecret(sessionSecret)
+    const validSecret = constantTimeEqual(tokenSecretHash, session.secretHash)
+
+    if (!validSecret)
+        return null
+
+    return session
+}
+
+async function getSession(db: PrismaClient, sessionId:  string): Promise<ISession | null> {
+    const now = new Date()
+
+    const session = await db.session.findUnique({ where: { id: sessionId } })
+
+    if (!session || !session.id)
+        return null
+
+    if (now.getTime() - session.createAt.getTime() >= SESSION_EXPIRES_IN_SECONDS * 1000) {
+        await deleteSession(db, sessionId)
+        return null
+    }
+
+    return {
+        id: session.id,
+        secretHash: session.secretHash,
+        createdAt: session.createAt
+    }
+}
+
+async function deleteSession(db: PrismaClient, sessionId:  string): Promise<void> {
+    await db.session.delete({where: { id: sessionId }})
+}
+
+function generateSecureRandomString(): string {
+    const alphabet = "abcdefghijkmnpqrstuvwxyz23456789";
+
+    const bytes = new Uint8Array(24)
+    crypto.getRandomValues(bytes)
+
+    let id = ''
+    for (let i = 0; i < bytes.length; i++) {
+        id += alphabet[bytes[i] >> 3]
+    }
+
+    return id
+}
+
+async function hashSecret(secret : string): Promise<string> {
+    const secretBytes = new TextEncoder().encode(secret)
+    const secretHashBuffer = await crypto.subtle.digest('SHA-256', secretBytes)
+    return new Uint8Array(secretHashBuffer).toBase64()
+}
+
+function constantTimeEqual(a: string, b: string): boolean {
+    const a8 = Uint8Array.fromBase64(a)
+    const b8 = Uint8Array.fromBase64(b)
+
+    if (a8.byteLength !== b8.byteLength)
+        return false
+
+    let c = 0
+    for (let i = 0; i < a8.byteLength; i++) {
+        c |= a8[i] ^ b8[i]
+    }
+
+    return c === 0
+}
